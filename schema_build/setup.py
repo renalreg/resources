c175ed2d745eae9e3c10e2f454fa90fa2e8b5bb7
Roger Day <roger.day@renalregistry.nhs.uk>
2017-11-29 16:06:39 +0000

    Add scaffolding for building pip from XSD

---
+++ schema_build/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+from setuptools import find_packages
+import re
+
+with open('ukrdc_schema/__init__.py', 'r') as f:
+    match_pat = r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]'
+    version = re.search(match_pat, f.read(), re.MULTILINE).group(1)
+
+
+setup(
+    name='ukrdc_schema',
+    version=version,
+    long_description='Built schema for the ukrdc',
+    author='UK Renal Registry',
+    author_email='rrsystems@renalregistry.nhs.uk',
+    url='https://www.renalreg.org/',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=True,
+)
