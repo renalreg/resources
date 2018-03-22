68245fc0f2818af6fda411cbe83684ddc94d28ce
Roger Day <roger.day@renalregistry.nhs.uk>
2018-03-22 16:25:05 +0000

    use the correct name for the db

--- scripts/schema_report.py
+++ scripts/schema_report.py
@@ -223,7 +223,7 @@ def make_report(db_metadata, filepath):
 
 def main():
     xsd_path = 'schema/ukrdc/'
-    conn = Connection.create_psycopg2_connection(dbkey='ukrdc3')
+    conn = Connection.create_psycopg2_connection(dbkey='ukrdc3-tunnel')
     dbm = DBMetadata(conn.cursor(), xsd_path)
     dbm.run()
     make_report(
