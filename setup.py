4b61ee308d96546befb02e087b26ff01d25387ea
George Swinnerton <george.swinnerton@renalregistry.nhs.uk>
2021-06-17 11:12:20 +0100

    Add setup.py#

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="resources",
+    version="0.1",
+    author="UK Renal Registry",
+    author_email="rrsystems@renalregistry.nhs.uk",
+    url="https://www.renalreg.org/",
+    packages=[
+        "resources",
+    ],
+    zip_safe=True
+)
