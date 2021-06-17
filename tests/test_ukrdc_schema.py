8bf8701abe64cbd148ffbb28fc966153639c9cda
George Swinnerton <george.swinnerton@renalregistry.nhs.uk>
2021-06-17 11:59:53 +0100

    Fix Tests

--- tests/test_ukrdc_schema.py
+++ tests/test_ukrdc_schema.py
@@ -5,32 +5,44 @@ from lxml import etree
 
 class TestUKRDCSchema(TestCase):
     def setUp(self):
- 
-        xmlschema_doc = etree.parse(open('../schema/ukrdc/UKRDC.xsd', 'r'))
+
+        xmlschema_doc = etree.parse(open("schema/ukrdc/UKRDC.xsd", "r"))
         self.ukrdc_xmlschema = etree.XMLSchema(xmlschema_doc)
 
     def test_sample_file(self):
 
-        xml_doc = etree.parse(open('../sample_files/ukrdc/ukrdc.xml', 'r', encoding='utf-8'))
+        import os
+
+        print("XXX")
+        print(os.getcwd())
+        print("XXX")
+        xml_doc = etree.parse(
+            open("sample_files/ukrdc/ukrdc.xml", "r", encoding="utf-8")
+        )
         self.ukrdc_xmlschema.validate(xml_doc)
-        
+
         tree = etree.ElementTree(xml_doc.getroot())
-        
+
         errors_found = False
-        
-        for i, error in enumerate(xmlschema.error_log):
-        
+
+        for i, error in enumerate(self.ukrdc_xmlschema.error_log):
+
             errors_found = True
-        
+
             line_number = error.line
             column_number = error.column
             error_message = error.message
 
-            for e in tree.xpath('.//*'):
+            xml_path = None
+
+            for e in tree.xpath(".//*"):
                 if line_number == e.sourceline:
-                    print("Error: ", i)
-                    print(tree.getpath(e))
-                    print(error.message)
-                    
-                    
+                    xml_path = tree.getpath(e)
+
+            print("Error: ", i)
+            print(error_message)
+            print(f"Line Number {line_number} Column Number: {column_number}")
+            if xml_path:
+                print(xml_path)
+
         self.assertFalse(errors_found)
