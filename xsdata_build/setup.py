45c6debd2d418d7f179f50ba2360ca7c33764d65
Joel Collins <joel.collins@renalregistry.nhs.uk>
2022-01-31 12:42:15 +0000

    Added README with XSData examples

--- xsdata_build/setup.py
+++ xsdata_build/setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup
 from setuptools import find_packages
+from pathlib import Path
+
+# Read the contents of the README file
+long_description = (Path(__file__).parent / "README.md").read_text()
 
 setup(
-    name="ukrdc_xsdata",
-    version="3.3.0-post.1",
-    long_description="XSData models for the UKRDC",
+    name="ukrdc-xsdata",
+    version="3.3.0.post1",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author="UK Renal Registry",
     author_email="rrsystems@renalregistry.nhs.uk",
     url="https://www.renalreg.org/",
