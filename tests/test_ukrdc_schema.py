096d4b497310bd992015535346ba55737d90c30d
Joel Collins <joel.collins@renalregistry.nhs.uk>
2022-07-16 09:00:43 +0000

    Test all sample files

--- tests/test_ukrdc_schema.py
+++ tests/test_ukrdc_schema.py
@@ -1,4 +1,5 @@
 from unittest import TestCase
+import glob
 
 from lxml import etree
 
@@ -9,35 +10,36 @@ class TestUKRDCSchema(TestCase):
         xmlschema_doc = etree.parse(open("schema/ukrdc/UKRDC.xsd", "r"))
         self.ukrdc_xmlschema = etree.XMLSchema(xmlschema_doc)
 
-    def test_sample_file(self):
+    def test_sample_files(self):
 
-        xml_doc = etree.parse(
-            open("sample_files/ukrdc/ukrdc.xml", "r", encoding="utf-8")
-        )
-        self.ukrdc_xmlschema.validate(xml_doc)
+        for sample_path in glob.glob("sample_files/ukrdc/*.xml"):
+            xml_doc = etree.parse(open(sample_path, "r", encoding="utf-8"), None)
+            self.ukrdc_xmlschema.validate(xml_doc)
 
-        tree = etree.ElementTree(xml_doc.getroot())
+            tree = etree.ElementTree(xml_doc.getroot())
 
-        errors_found = False
+            errors_found = False
 
-        for i, error in enumerate(self.ukrdc_xmlschema.error_log):
+            for i, error in enumerate(self.ukrdc_xmlschema.error_log):
 
-            errors_found = True
+                errors_found = True
 
-            line_number = error.line
-            column_number = error.column
-            error_message = error.message
+                line_number = error.line
+                column_number = error.column
+                error_message = error.message
 
-            xml_path = None
+                xml_path = None
 
-            for e in tree.xpath(".//*"):
-                if line_number == e.sourceline:
-                    xml_path = tree.getpath(e)
+                for e in tree.xpath(".//*"):
+                    if line_number == e.sourceline:
+                        xml_path = tree.getpath(e)
 
-            print("Error: ", i)
-            print(error_message)
-            print(f"Line Number {line_number} Column Number: {column_number}")
-            if xml_path:
-                print(xml_path)
+                print("Error: ", i)
+                print(error_message)
+                print(
+                    f"{sample_path} / Line Number: {line_number} Column Number: {column_number}"
+                )
+                if xml_path:
+                    print(xml_path)
 
-        self.assertFalse(errors_found)
+            self.assertFalse(errors_found)
