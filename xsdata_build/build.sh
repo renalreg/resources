4517c81b1f27e064b087a76a0e015f55bb94b9b8
OliveraReeves <158471173+OliveraReeves@users.noreply.github.com>
2025-07-22 14:04:16 +0100

    added pytyped (#30)
    * added pytyped
    
    * added pytyped to build

--- xsdata_build/build.sh
+++ xsdata_build/build.sh
@@ -1,3 +1,6 @@
 xsdata ../schema/ukrdc/UKRDC.xsd --config .xsdata.xml --package ukrdc_xsdata.ukrdc
 xsdata ../schema/pv2/PV_2_0.xsd --config .xsdata.xml --package ukrdc_xsdata.pv
 xsdata ../schema/rrtf/RRTF_4.xsd --config .xsdata.xml --package ukrdc_xsdata.rrtf
+
+# Add py.typed to top-level package to enable type checking
+touch ukrdc_xsdata/py.typed
